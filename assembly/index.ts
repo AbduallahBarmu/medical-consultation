90fa4c2743b410869d19e51b537c00bdc0e3878b
unknown <Abduallah.imu2@gmail.com>
2022-05-17 15:21:56 +0300

    create consMessage method

--- assembly/index.ts
+++ assembly/index.ts
@@ -6,19 +6,39 @@ import {ConsultationModule} from './modules/consultationModule'
 
 
 
-
 @nearBindgen
 export class DocCons{
 
+  // storage info in blockchaing
+  messageLists:PersistentVector<ConsultationModule> = new PersistentVector<ConsultationModule>('w')
  
- 
-
   // method create Doctor account (doctor storage )
   
 
 
-  // method create consultation message(Patient =>  doctor by doctor ID )
-   
+  // method create consultation message(Patient => doctor by doctor ID )
+  consMessage(message:string , receiver:string):ConsultationModule{
+      let sender:string = Context.sender ; 
+      let writing:ConsultationModule = new ConsultationModule(message, sender, receiver);
+      this.messageLists.push(writing)  // anyone call this method , will take the object and save it in the list 
+      
+      return writing ; 
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   // method list messages for the doctor  
